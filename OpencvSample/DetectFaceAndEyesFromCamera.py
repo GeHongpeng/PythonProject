412944b600b94a70fdb664a1183d33f4dd656adc
GeHongpeng <ge.hongpeng@gmail.com>
2016-03-08 16:51:59 +0900

    Create DetectFaceAndEyesFromCamera.py

---
+++ OpencvSample/DetectFaceAndEyesFromCamera.py
@@ -0,0 +1,25 @@
+import numpy as np
+import cv2
+
+face_cascade = cv2.CascadeClassifier('/usr/local/Cellar/opencv/2.4.12_2/share/OpenCV/haarcascades/haarcascade_frontalface_alt.xml')
+eye_cascade = cv2.CascadeClassifier('/usr/local/Cellar/opencv/2.4.12_2/share/OpenCV/haarcascades/haarcascade_eye.xml')
+
+while True:
+    ret, img = cap.read()
+    
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x,y,w,h) in faces:
+      img = cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+      roi_gray = gray[y:y+h, x:x+w]
+      roi_color = img[y:y+h, x:x+w]
+      eyes = eye_cascade.detectMultiScale(roi_gray)
+      for (ex,ey,ew,eh) in eyes:
+        cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+
+    cv2.imshow('Video', img)
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()

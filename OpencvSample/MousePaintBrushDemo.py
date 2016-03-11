1fa2853b35008c1fb1a4d7001a7534b44918cd2d
GeHongpeng <ge.hongpeng@gmail.com>
2016-03-11 16:12:32 +0900

    Update MousePaintBrushDemo.py

--- OpencvSample/MousePaintBrushDemo.py
+++ OpencvSample/MousePaintBrushDemo.py
@@ -45,7 +45,7 @@ cv2.setMouseCallback('image',draw_circle)
 
 while(1):
     cv2.imshow('image',img)
-    k=cv2.waitKey(1)&0xFF
+    k = cv2.waitKey(1) & 0xFF
     if k == ord('m'):
         mode = not mode
     elif k == 27:

68759b2bacfc66aab6b351d4f6aa5dbf1098f15e
GeHongpeng <ge.hongpeng@gmail.com>
2016-03-11 15:39:23 +0900

    Update MouseAsAPaintBrush.py

--- OpencvSample/MouseAsAPaintBrush.py
+++ OpencvSample/MouseAsAPaintBrush.py
@@ -3,22 +3,22 @@
 import cv2
 import numpy as np
 
-events=[i for i in dir(cv2) if 'EVENT'in i]
-print events
+events = [i for i in dir(cv2) if 'EVENT' in i]
+print(events)
 
-#mouse callback function
+# mouse callback function
 def draw_circle(event,x,y,flags,param):
-    if event==cv2.EVENT_LBUTTONDBLCLK:
-        cv2.circle(img,(x,y),100,(0,255,0),-1)
+    if event == cv2.EVENT_LBUTTONDBLCLK:
+        cv2.circle(img, (x, y), 100, (0, 255, 0), -1)
 
- # Create a black image, a window and bind the function to window
-img=np.zeros((512,512,3),np.uint8)
+# Create a black image, a window and bind the function to window
+img=np.zeros((512, 512, 3),np.uint8)
 cv2.namedWindow('image')
 cv2.setMouseCallback('image',draw_circle)
 
 while(1):
     cv2.imshow('image',img)
-    if cv2.waitKey(20)&0xFF==27:
+    if cv2.waitKey(20) & 0xFF == 27:
         break
 
 cv2.destroyAllWindows()

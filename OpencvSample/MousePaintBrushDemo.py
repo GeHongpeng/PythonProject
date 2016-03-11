d0b0fc18a9231e34555fdfa03350f82d1ce704d5
GeHongpeng <ge.hongpeng@gmail.com>
2016-03-11 16:18:00 +0900

    Update MousePaintBrushDemo.py

--- OpencvSample/MousePaintBrushDemo.py
+++ OpencvSample/MousePaintBrushDemo.py
@@ -3,33 +3,34 @@
 import cv2
 import numpy as np
 
-# 当鼠标按下时变为True
+# left-button down → True
 drawing = False
 
-# 如果mode 为true 绘制矩形。按下'm' 变成绘制曲线。
+"""
+mode: true → rectangle
+mode: false →  circle
+Press 'm' to change mode
+"""
 mode = True
 ix,iy = -1,-1
 
-# 创建回调函数
+# Callback Function
 def draw_circle(event,x,y,flags,param):
     global ix, iy, drawing, mode
 
-    # 当按下左键是返回起始位置坐标
     if event == cv2.EVENT_LBUTTONDOWN:
         drawing = True
         ix,iy = x,y
-    # 当鼠标左键按下并移动是绘制图形。event 可以查看移动，flag 查看是否按下
     elif event == cv2.EVENT_MOUSEMOVE and flags == cv2.EVENT_FLAG_LBUTTON:
         if drawing == True:
             if mode == True:
                 cv2.rectangle(img, (ix, iy), (x, y), (0, 255, 0), -1)
             else:
-                # 绘制圆圈，小圆点连在一起就成了线，3 代表了笔画的粗细
                 #cv2.circle(img,(x,y),3,(0,0,255),2)
-                # 下面的代码是起始点为圆心，起点到终点为半径的
+                
                 r = int(np.sqrt((x - ix) ** 2 + (y - iy) ** 2))
                 cv2.circle(img, (x, y), r, (0, 0, 255), -1)
-    # 当鼠标松开停止绘画。
+    # left-button up, draw the rectangle or circle
     elif event == cv2.EVENT_LBUTTONUP:
         drawing == False
         #if mode==True:

b8e717c6b9432d826183e309daa4c30d413f27c2
GeHongpeng <ge.hongpeng@gmail.com>
2016-03-25 14:57:13 +0900

    Update GeometricTransformationsofImages.py

--- OpencvSample/Chapter5_Image_Processing_in_OpenCV/GeometricTransformationsofImages.py
+++ OpencvSample/Chapter5_Image_Processing_in_OpenCV/GeometricTransformationsofImages.py
@@ -57,7 +57,7 @@ cv2.destroyAllWindows()
 Affine Transformation
 """
 img = cv2.imread('./data/lion.jpg')
-rows,cols,ch = img.shape
+rows, cols, ch = img.shape
 
 pts1 = np.float32([[50, 50], [200, 50], [50, 200]])
 pts2 = np.float32([[10, 100], [200, 50], [100, 250]])
@@ -74,7 +74,7 @@ plt.show()
 Perspective Transformation
 """
 img = cv2.imread('./data/lion.jpg')
-rows,cols,ch = img.shape
+rows, cols, ch = img.shape
 
 pts1 = np.float32([[56, 65], [368, 52],[28, 387], [389, 390]])
 pts2 = np.float32([[0, 0], [300, 0], [0, 300], [300, 300]])

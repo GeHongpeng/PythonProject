a487bb333210677b9d45b0aeed8b0887ecd4db2b
GeHongpeng <ge.hongpeng@gmail.com>
2016-05-17 17:15:21 +0900

    Update SettingFromImageDialog.py

--- PyQt/SettingFromImageDialog.py
+++ PyQt/SettingFromImageDialog.py
@@ -1,11 +1,5 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'MyPyQtTest.ui'
-#
-# Created by: PyQt4 UI code generator 4.11.4
-#
-# WARNING! All changes made in this file will be lost!
-
 import sys
 from PyQt4 import QtCore, QtGui
 

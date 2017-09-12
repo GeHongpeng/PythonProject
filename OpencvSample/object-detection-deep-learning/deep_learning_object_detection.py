60fc21cfdeac460f40861cae33045d0b4540f7bd
GeHongpeng <ge.hongpeng@gmail.com>
2017-09-12 12:06:55 +0900

    Update object-detection-deep-learning.py

--- OpencvSample/object-detection-deep-learning/deep_learning_object_detection.py
+++ OpencvSample/object-detection-deep-learning/deep_learning_object_detection.py
@@ -5,6 +5,7 @@
 import numpy as np
 import argparse
 import cv2
+import time
 
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
@@ -36,20 +37,20 @@ blob = cv2.dnn.blobFromImage(image, 0.007843, (300, 300), 127.5)
 # predictions
 print("[INFO] computing object detections...")
 net.setInput(blob)
+start = time.time()
 detections = net.forward()
+end = time.time()
+print("[INFO] classification took {:.5} seconds".format(end - start))
 
 # loop over the detections
 for i in np.arange(0, detections.shape[2]):
-    # extract the confidence (i.e., probability) associated with the
-    # prediction
+    # extract the confidence (i.e., probability) associated with the prediction
     confidence = detections[0, 0, i, 2]
 
-    # filter out weak detections by ensuring the `confidence` is
-    # greater than the minimum confidence
+    # filter out weak detections by ensuring the `confidence` is greater than the minimum confidence
     if confidence > args["confidence"]:
         # extract the index of the class label from the `detections`,
-        # then compute the (x, y)-coordinates of the bounding box for
-        # the object
+        # then compute the (x, y)-coordinates of the bounding box for the object
         idx = int(detections[0, 0, i, 1])
         box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
         (startX, startY, endX, endY) = box.astype("int")
@@ -57,8 +58,7 @@ for i in np.arange(0, detections.shape[2]):
         # display the prediction
         label = "{}: {:.2f}%".format(CLASSES[idx], confidence * 100)
         print("[INFO] {}".format(label))
-        cv2.rectangle(image, (startX, startY), (endX, endY),
-            COLORS[idx], 2)
+        cv2.rectangle(image, (startX, startY), (endX, endY), COLORS[idx], 2)
         y = startY - 15 if startY - 15 > 15 else startY + 15
         cv2.putText(image, label, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
 
